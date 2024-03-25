ff3884ab16826c1ba72a4ec35c6a7feda988001d
jonasdevpro <kjonasdevpro@gmail.com>
2024-03-25 18:12:32 +0100

    package swiper

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,4 @@
-// home.page.ts 
 import { Component } from '@angular/core';
-
 
 @Component({
   selector: 'app-home',
@@ -8,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-   
+
   constructor() {}
 
 }
