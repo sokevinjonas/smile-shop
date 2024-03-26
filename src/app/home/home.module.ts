a19bef25deb415ab568e617911b9009b146c07a3
jonasdevpro <kjonasdevpro@gmail.com>
2024-03-26 03:31:48 +0100

    slide reussit + category

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -7,13 +7,15 @@ import { HomePage } from './home.page';
 import { HomePageRoutingModule } from './home-routing.module';
 
 
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
   ],
-  declarations: [HomePage]
+  declarations: [HomePage],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class HomePageModule {}
